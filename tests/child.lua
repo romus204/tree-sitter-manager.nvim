3d0bdbed3fd99dbe63b0e729b13782e1dc9c42b4
tunaflsh <tunaflish.ai@gmail.com>
2026-05-20 16:35:00 +0200

    test: improve parser_wait timing

--- tests/child.lua
+++ tests/child.lua
@@ -38,39 +38,33 @@ function M:update_config(config)
     return vim.deepcopy(self.config, true)
 end
 
-function M:parser_wait(languages, timeout, interval)
+function M:parser_wait(languages, timeout)
     timeout = timeout or 60000
-    interval = interval or 100
     self.lua([[
     local util = require("tree-sitter-manager.util")
-    _G.success = true
-    for _, lang in ipairs(]] .. vim.inspect(languages) .. [[) do
-        local success, reason = vim.wait(
-            ]] .. timeout .. [[,
-            function()
-                return vim.uv.fs_stat(util.ppath(lang))
-            end,
-            ]] .. interval .. [[
-        )
-        if not success then
-            _G.success = false
-            _G.reason = reason
-            _G.language = lang
-            break
-        end
-    end
+    languages = ]] .. vim.inspect(languages) .. [[
+    success, reason = vim.wait(
+        ]] .. timeout .. [[,
+        function()
+            languages = vim.tbl_filter(function(lang)
+                return not vim.uv.fs_stat(util.ppath(lang))
+            end, languages)
+            return #languages == 0
+        end,
+        100
+    )
     vim.wait(500)
     ]])
-    local success = self.lua_get("_G.success")
-    local reason = self.lua_get("_G.reason")
-    local language = self.lua_get("_G.language")
+    local success = self.lua_get("success")
+    local reason = self.lua_get("reason")
+    local langs = self.lua_get("languages")
     if not success then
         if -1 == reason then
             reason = "timeout"
         elseif -2 == reason then
             reason = "interrupt"
         end
-        error(reason .. ": installing parser " .. language)
+        error(reason .. " installing parser " .. langs)
     end
 end
 
