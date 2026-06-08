d7d264da880b510a181b1d6253011631e8b5acdc
tunaflsh <tunaflish.ai@gmail.com>
2026-06-08 10:38:31 +0200

    test: add test case for #99

---
+++ tests/test_assume_installed.lua
@@ -0,0 +1,56 @@
+local languages = _G.languages or { "c", "lua", "markdown", "markdown_inline", "query", "vim", "vimdoc" }
+local child = require("tests.child")
+local config = require("tree-sitter-manager.config")
+local util = require("tree-sitter-manager.util")
+local requiredby = vim.iter(languages):fold({}, function(acc, lang)
+    for _, dep in ipairs(config.languages) do
+        if vim.list_contains(util.get_requires(dep), lang) then
+            acc[lang] = dep
+            break
+        end
+    end
+    return acc
+end)
+local install_list = { unpack(languages), unpack(vim.tbl_values(requiredby)) }
+
+local T = MiniTest.new_set({
+    hooks = {
+        pre_once = function()
+            child:setup({
+                assume_installed = languages,
+                ensure_installed = install_list,
+            })
+        end,
+        post_once = function()
+            child:cleanup()
+        end,
+    },
+    parametrize = vim.iter(languages):fold({}, function(acc, lang)
+        table.insert(acc, { lang, false })
+        local dep = requiredby[lang]
+        if dep then
+            table.insert(acc, { lang, dep })
+        end
+        return acc
+    end),
+})
+
+T["assume_installed"] = function(lang, dep)
+    -- parser for lang should already be installed
+    child:wait({ lang }, 0)
+    -- verify installation for the dependant language
+    if dep then
+        child:wait({ dep })
+    end
+end
+
+T["query"] = MiniTest.new_set({
+    parametrize = { { "highlights" } },
+})
+T["query"]["get"] = function(lang, dep, query)
+    -- check that treesitter can access queries
+    lang = dep or lang
+    eq(true, child.lua_get("nil ~= vim.treesitter.query.get('" .. lang .. "', '" .. query .. "')"))
+end
+
+return T
