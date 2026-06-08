3174eb3b4a78e276a2cd2b22a465b95a8e1edf53
tunaflsh <tunaflish.ai@gmail.com>
2026-06-08 08:21:51 +0200

    test: auto_install, noauto_install #98

--- tests/test_auto_install.lua
+++ tests/test_auto_install.lua
@@ -1,29 +1,45 @@
-local languages = _G.languages or { "terraform", "ocaml", "helm", "python", "javascript", "razor" }
+local languages = _G.languages or { "tsv", "javascript" }
+local filetypes = require("tree-sitter-manager.filetypes")
+local util = require("tree-sitter-manager.util")
 local child = require("tests.child")
 
 local T = MiniTest.new_set({
     hooks = {
-        pre_once = function()
-            child:setup({ auto_install = true })
-            for _, lang in ipairs(languages) do
-                child.cmd("se ft=" .. lang)
-            end
-            child.cmd("se ft=")
+        pre_case = function()
+            child:setup({ auto_install = true, noauto_install = languages })
         end,
-        post_once = function()
+        post_case = function()
             child:cleanup()
         end,
     },
-    parametrize = vim.iter(languages)
-        :map(function(lang)
-            return { lang }
-        end)
-        :totable(),
+    parametrize = vim.iter(languages):fold({}, function(acc, lang)
+        table.insert(acc, { lang, lang })
+        for _, ft in ipairs(filetypes[lang] or {}) do
+            table.insert(acc, { lang, ft })
+        end
+        return acc
+    end),
 })
 
-T["auto_install"] = function(lang)
+T["noauto_install"] = function(lang, ft)
+    child.cmd("se ft=" .. ft)
+    -- expect timeout because no parser is installed
+    er(function()
+        child:wait({ lang }, 4000)
+    end, "timeout")
+end
+
+T["auto_install"] = MiniTest.new_set({
+    hooks = {
+        pre_case = function()
+            child:update({ noauto_install = {} })
+        end,
+    },
+})
+T["auto_install"]["success"] = function(lang, ft)
+    child.cmd("se ft=" .. ft)
     -- wait for the parser to successfully install
-    child:wait({ lang })
+    child:wait({ lang }, 4000)
 end
 
 return T
