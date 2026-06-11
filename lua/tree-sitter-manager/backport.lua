c1ba097e797822b1aedf8f65ed7d106ee66455bf
tunaflsh <tunaflish.ai@gmail.com>
2026-06-11 04:15:00 +0200

    fix(backport): return the module

--- lua/tree-sitter-manager/backport.lua
+++ lua/tree-sitter-manager/backport.lua
@@ -1,10 +1,14 @@
 local installer = require("tree-sitter-manager.installer")
 local ui = require("tree-sitter-manager.ui")
 
-M._install_single = function(lang, callback)
-    installer.install(lang, function(out)
-        callback(out.ok)
-    end, true, true)
-end
-M.open = ui.open
-M._act = ui._act
+local M = {
+    _install_single = function(lang, callback)
+        installer.install(lang, function(out)
+            callback(out.ok)
+        end, true, true)
+    end,
+    open = ui.open,
+    _act = ui._act,
+}
+
+return M
