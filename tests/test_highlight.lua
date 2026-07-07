e39182217d71747b9d68c287595e736185f8d557
tunaflsh <tunaflish.ai@gmail.com>
2026-07-07 05:57:34 +0200

    fix(test_highlight): built-in languages are always active

--- tests/test_highlight.lua
+++ tests/test_highlight.lua
@@ -1,5 +1,7 @@
 local languages = _G.languages or { "tsv", "tsx" }
 
+local builtin = require("tree-sitter-manager.built-in-languages")
+
 local T = new_set({
     hooks = {
         pre_once = function()
@@ -12,7 +14,11 @@ local T = new_set({
 T.before_install = function(ft)
     -- no highlighter before installation
     child.cmd("e name." .. ft .. "|se ft=" .. ft)
-    eq(false, child.lua_get("nil ~= vim.treesitter.highlighter.active[vim.fn.bufnr()]"))
+    local lang = vim.treesitter.language.get_lang(ft)
+    eq(
+        vim.list_contains(builtin.languages, lang),
+        child.lua_get("nil ~= vim.treesitter.highlighter.active[vim.fn.bufnr()]")
+    )
 end
 
 T.after_install = MiniTest.new_set({
@@ -49,7 +55,10 @@ T.nohighlight = MiniTest.new_set({
 T.nohighlight.fail = function(ft)
     -- expect no highlighting for any languages
     child.cmd("enew|set ft=" .. ft)
-    eq(false, child.lua_get("nil ~= vim.treesitter.highlighter.active[vim.fn.bufnr()]"))
+    eq(
+        vim.list_contains(builtin.highlight, ft),
+        child.lua_get("nil ~= vim.treesitter.highlighter.active[vim.fn.bufnr()]")
+    )
 end
 
 return T
